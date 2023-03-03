0878e4b9504cd736d145a60cc0ade2a04bc0891c
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-03 17:24:59 -0500

    loops questions

---
+++ src/Practice_Questions_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Practice_Questions_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter thr rows : ");
+        int i = sc.nextInt();
+
+        System.out.println("Enter thr column : ");
+        int j = sc.nextInt();
+
+        for (int a = 1; a <= i; a++){
+            for (int b = 1; b <= j; b++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
