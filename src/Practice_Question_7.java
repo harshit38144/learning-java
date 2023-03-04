b595f6753706df6cee6a2f3668fc8487e4875314
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-04 15:04:56 -0500

    question

---
+++ src/Practice_Question_7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Practice_Question_7 {
+    public static void main(String[] args) {
+
+        //Print:
+        //1 2 3 4 5 6
+        //1 2 3 4 5
+        //1 2 3 4
+        //1 2 3 
+        //1 2
+        //1
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of columns: ");
+        int n = sc.nextInt();
+
+        //Outer loop
+        for (int i = 1; i <= n; i++){
+            //Inner loop
+            for (int j = 1; j <= n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
