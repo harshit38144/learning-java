587ec22ef85bbdd72939bd87d92f3dffb12dfa3b
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-04 10:50:22 -0500

    some questions in loop

---
+++ src/Practice_Question_4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Practice_Question_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Columns: ");
+        int i = sc.nextInt();
+
+        //Outer loop
+        for (int a = 1; a<=i; a++){
+            //Inner loop
+            for (int b = 1; b<=a; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
