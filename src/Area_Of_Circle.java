7b9e403744eccd3a042127f5d3d67e5831fb0def
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-19 13:37:04 -0500

    Area of circle

---
+++ src/Area_Of_Circle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Area_Of_Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float pie = 3.141592653f;
+        System.out.println("Enter radius of circle: ");
+        float a = sc.nextInt();
+        float b = a*a;
+        float c = pie*b;
+        System.out.println("Area of circle is : ");
+        System.out.println(c);
+    }
+}
