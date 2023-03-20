96c09405ee05b2ba6a95d7b173a4facb524d3ace
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-20 00:56:52 -0500

    perimeter of equilateral triangle

---
+++ src/Perimeter_Of_Equilateral_Triangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Perimeter_Of_Equilateral_Triangle {
+    public static void main(String[] args) {
+        //Perimeter Of Equilateral_Triangle..............................
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sides of triangle : ");
+        int a = sc.nextInt();
+        int p = 3*a;
+        System.out.println("Perimeter Of Equilateral_Triangle is : "+p);
+    }
+}
