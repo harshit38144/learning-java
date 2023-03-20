9c218ff9d7b99dfe1d70fb6f147cd2cf795c7776
Harshit Kumar <kumarharshit6206@gmail.com>
2023-03-20 01:03:52 -0500

    perimeter of parallelogram

---
+++ src/Perimeter_Of_Parallelogram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Perimeter_Of_Parallelogram {
+    public static void main(String[] args) {
+        //Perimeter Of Parallelogram.................................
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base of Parallelogram : ");
+        int b = sc.nextInt();
+        System.out.println("Enter side of Parallelogram : ");
+        int s = sc.nextInt();
+        int POP = 2*(b+s);
+        System.out.println("Perimeter Of Equilateral_Triangle is : "+POP);
+
+    }
+}
