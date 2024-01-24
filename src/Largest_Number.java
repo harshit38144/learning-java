ece7b102740088a020593007d02151582afa9bfa
Harshit Kumar <106590879+harshit38144@users.noreply.github.com>
2024-01-24 10:25:29 +0000

    some changes

--- src/Largest_Number.java
+++ src/Largest_Number.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class Largest_Number {
     public static void main(String[] args) {
-        //Take 2 numbers as input and print the largest number.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first no. : ");
-        int a = sc.nextInt();
-        System.out.println("Enter Second no. : ");
-        int b = sc.nextInt();
-        if(a>b){
-            System.out.println("Largest number is : "+a);
-        }else{
-            System.out.println("Largest number is : "+b);
+        try (//Take 2 numbers as input and print the largest number.
+        Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter first no. : ");
+            int a = sc.nextInt();
+            System.out.println("Enter Second no. : ");
+            int b = sc.nextInt();
+            if(a>b){
+                System.out.println("Largest number is : "+a);
+            }else{
+                System.out.println("Largest number is : "+b);
+            }
         }
     }
 }
